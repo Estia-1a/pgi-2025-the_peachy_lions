a0ba64d8fcfdbb2d06b37b38ce720f502fe82264
elouan-rousseau <189098961+elouan-rousseau@users.noreply.github.com>
2025-05-21 17:28:19 +0200

    Ajout des fonctions du tutoriel

--- src/features.c
+++ src/features.c
@@ -14,3 +14,53 @@
 void helloWorld() {
     printf("Hello World !");
 }
+void dimension (char *source_path) {
+    int width, height, channel_count;
+    unsigned char *data;
+    int result = read_image_data(source_path, &data, &width, &height, &channel_count);
+    if (result) {
+        printf("Dimension: %d, %d", width, height);
+    } else {
+        printf("ERREUR !");
+    }
+}
+void first_pixel(char *source_path) {
+    int width, height, channel_count, R, G, B;
+    unsigned char *data;
+    int result = read_image_data(source_path, &data, &width, &height, &channel_count);
+    if (result) {
+        R = data[0];
+    G = data[1];
+    B = data[2];
+    printf("first_pixel : %d, %d, %d", R, G, B);
+    } else {
+        printf("ERREUR !");
+    }
+}
+void tenth_pixel(char *source_path) {
+    int width, height, channel_count, R, G, B, n;
+    n = 10;
+    unsigned char *data;
+    int result = read_image_data(source_path, &data, &width, &height, &channel_count);
+    if (result) {
+    R = data[3*n];
+    G = data[3*n+1];
+    B = data[3*n+2];
+    printf("tenth_pixel : %d, %d, %d", R, G, B);
+    } else {
+        printf("ERREUR !");
+    }    
+}
+void second_line(char *source_path) {
+    int width, height, channel_count, R, G, B;
+    unsigned char *data;
+    int result = read_image_data(source_path, &data, &width, &height, &channel_count);
+    if (result) {
+    R = data[3*width];
+    G = data[3*width+1];
+    B = data[3*width+2];
+    printf("second_line : %d, %d, %d", R, G, B);
+    } else {
+        printf("ERREUR !");
+    } 
+}
--- src/utils.c
+++ src/utils.c
@@ -2,8 +2,5 @@
 
 #include "utils.h"
 
-/**
- * @brief Here, you have to define functions of the pixel struct : getPixel and setPixel.
- * 
- */
+
 
