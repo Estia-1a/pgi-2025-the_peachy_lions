ebea65532957aaf421b20ab9aa8fe1db5f8c1851
LodelciaJ <189098689+LodelciaJ@users.noreply.github.com>
2025-05-21 17:28:19 +0200

    ajout des fonctions du tutoriel

--- src/utils.c
+++ src/utils.c
@@ -6,4 +6,9 @@
  * @brief Here, you have to define functions of the pixel struct : getPixel and setPixel.
  * 
  */
-
+if (img == NULL || img->data == NULL) {
+    return NULL
+}
+if (x < 0 || x >= img ->width || y < 0 || y >= img->height) {
+    return NULL
+}
