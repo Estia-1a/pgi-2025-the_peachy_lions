d496d323b1d08defc4bfdc576369e266a3f1423f
LodelciaJ <189098689+LodelciaJ@users.noreply.github.com>
2025-06-13 10:47:48 +0200

    VF

--- src/main.c
+++ src/main.c
@@ -59,16 +59,7 @@ if ( strncmp( configuration.command, "min_component", 13 ) == 0 ) {
 }
 if ( strncmp( configuration.command, "max_component", 13 ) == 0 ) {
     max_component(configuration.arguments[0][0], configuration.filenames[0]);
-  }
-  if ( strncmp( configuration.command, "min_component_R", 15 ) == 0 ) {
-    min_component_R(configuration.filenames[0]);
-  }
-   if ( strncmp( configuration.command, "min_component_G", 15 ) == 0 ) {
-    min_component_G(configuration.filenames[0]);
-  }
-   if ( strncmp( configuration.command, "min_component_B", 15 ) == 0 ) {
-    min_component_B(configuration.filenames[0]);
-  }*/
+}
   if ( strncmp( configuration.command, "color_green", 11 ) == 0 ) {
     color_green( configuration.filenames[0] );
   }
